0dfa2ebda2701fcc4146c60f2babeecd98a50f80
Siniša Panić <sinisa.panic@gamanza.com>
2025-04-11 11:22:27 +0200

    Fix: remote obsolete

--- src/config.ts
+++ src/config.ts
@@ -9,31 +9,6 @@ interface ApplicationConfiguration {
   serviceName: string;
   host: string;
   port: number;
-  enableDbLogs: boolean;
-  vetoBridgeUrl: string;
-  vetoUsername: string;
-  vetoPassword: string;
-  vetoCheckOnLoginTimeoutSeconds: number;
-  vetoCheckOnRegistrationEnabled: boolean;
-  vetoCheckOnRegistrationMatchFnName: string;
-  vetoCheckOnLoginOptimizationEnabled: boolean;
-  vetoCheckOnLoginEnabled: boolean;
-  vetoCheckOnLoginMatchFnName: string;
-  manualVetoCheckEnabled: boolean;
-  manualVetoCheckMatchFnName: string;
-  playerCardUpdateVetoCheckEnabled: boolean;
-  playerCardUpdateVetoCheckMatchFnName: string;
-  ubiidVetoCheckEnabled: boolean;
-  ubiidVetoCheckMatchFnName: string;
-  rabbitMQ: {
-    host: string;
-    port: number;
-    user: string;
-    pass: string;
-    vhost: string;
-    protocol: string;
-  };
-  logRedactionPaths: string[];
 }
 
 export const config: ApplicationConfiguration = {
@@ -47,31 +22,6 @@ export const config: ApplicationConfiguration = {
   serviceName: process.env.SERVICE_NAME,
   host: process.env.HOST,
   port: Number(process.env.PORT),
-  enableDbLogs: process.env.ENABLE_DB_LOGS ? process.env.ENABLE_DB_LOGS.toLowerCase() === "true" : false,
-  vetoBridgeUrl: process.env.VETO_BRIDGE_URL,
-  vetoUsername: process.env.VETO_USERNAME,
-  vetoPassword: process.env.VETO_PASSWORD,
-  vetoCheckOnLoginTimeoutSeconds: Number(process.env.VETO_CHECK_ON_LOGIN_TIMEOUT_SECONDS),
-  vetoCheckOnRegistrationEnabled: process.env.VETO_CHECK_ON_REGISTRATION_ENABLED ? process.env.VETO_CHECK_ON_REGISTRATION_ENABLED.toLowerCase() === "true" : false,
-  vetoCheckOnRegistrationMatchFnName: process.env.VETO_CHECK_ON_REGISTRATION_MATCH_FN_NAME,
-  vetoCheckOnLoginOptimizationEnabled: process.env.VETO_CHECK_ON_LOGIN_OPTIMIZATION_ENABLED ? process.env.VETO_CHECK_ON_LOGIN_OPTIMIZATION_ENABLED.toLowerCase() === "true" : false,
-  vetoCheckOnLoginEnabled: process.env.VETO_CHECK_ON_LOGIN_ENABLED ? process.env.VETO_CHECK_ON_LOGIN_ENABLED.toLowerCase() === "true" : false,
-  vetoCheckOnLoginMatchFnName: process.env.VETO_CHECK_ON_LOGIN_MATCH_FN_NAME,
-  manualVetoCheckEnabled: process.env.MANUAL_VETO_CHECK_ENABLED ? process.env.MANUAL_VETO_CHECK_ENABLED.toLowerCase() === "true" : false,
-  manualVetoCheckMatchFnName: process.env.MANUAL_VETO_CHECK_MATCH_FN_NAME,
-  playerCardUpdateVetoCheckEnabled: process.env.PLAYER_CARD_UPDATE_VETO_CHECK_ENABLED ? process.env.PLAYER_CARD_UPDATE_VETO_CHECK_ENABLED.toLowerCase() === "true" : false,
-  playerCardUpdateVetoCheckMatchFnName: process.env.PLAYER_CARD_UPDATE_VETO_CHECK_MATCH_FN_NAME,
-  ubiidVetoCheckEnabled: process.env.UBIID_VETO_CHECK_ENABLED ? process.env.UBIID_VETO_CHECK_ENABLED.toLowerCase() === "true" : false,
-  ubiidVetoCheckMatchFnName: process.env.UBIID_VETO_CHECK_MATCH_FN_NAME,
-  rabbitMQ: {
-    protocol: process.env.RABBITMQ_PROTOCOL,
-    host: process.env.RABBITMQ_HOST,
-    port: Number(process.env.RABBITMQ_PORT),
-    user: process.env.RABBITMQ_USER,
-    pass: process.env.RABBITMQ_PASS,
-    vhost: process.env.RABBITMQ_VHOST,
-  },
-  logRedactionPaths: JSON.parse(process.env.LOG_REDACTION_PATHS),
 }
 
 /**
